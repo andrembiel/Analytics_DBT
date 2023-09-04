9242f24c36b063bed331f9eb95dfe6c01a5d5c0f
andrembiel@gmail.com <andrembiel@gmail.com>
2023-09-04 21:12:20 +0000

    added recover modules after deleted

---
+++ models/marts/marts_jaffle_shop/orders__payment_sucess.sql
@@ -0,0 +1,22 @@
+WITH stg_jaffle_shop__orders as (
+    
+    SELECT * FROM {{ ref('stg_jaffle_shop__orders') }}
+),
+
+stg_jaffle_shop__customers as (
+    
+    SELECT * FROM {{ ref('stg_jaffle_shop__customers') }}
+),
+
+int_successeful_payments__grouped_by_order_id as (
+    
+    SELECT * FROM {{ ref('int_successeful_payments__grouped_by_order_id') }}
+
+)
+
+SELECT * 
+
+FROM  stg_jaffle_shop__orders as orders
+LEFT JOIN stg_jaffle_shop__customers USING (customer_id)
+LEFT JOIN int_successeful_payments__grouped_by_order_id as payments 
+    ON orders.order_id = payments.order_id
