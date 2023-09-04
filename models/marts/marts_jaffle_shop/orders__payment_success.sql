5863c91bf6c624aeb749073e2f03232703ce99b6
andrembiel@gmail.com <andrembiel@gmail.com>
2023-09-04 22:40:22 +0000

    Adding the jaffle_shop models in the staging, intermediate and marts layers.

--- models/marts/marts_jaffle_shop/orders__payment_success.sql
+++ models/marts/marts_jaffle_shop/orders__payment_success.sql
@@ -12,15 +12,26 @@ int_successeful_payments__grouped_by_order_id as (
     
     SELECT * FROM {{ ref('int_successeful_payments__grouped_by_order_id') }}
 
-)
+), 
+
+orders__payment_success as (
 
 SELECT 
-orders.order_id
-,orders.customer_id
-,order_placed_at
-,orde
+    orders.order_id
+    ,orders.customer_id
+    ,order_placed_at
+    ,order_status
+    ,amount as total_amount
+    ,payment_finalised_at
+    ,customer_first_name
+    ,customer_last_name
 
 FROM  stg_jaffle_shop__orders as orders
-LEFT JOIN stg_jaffle_shop__customers USING (customer_id)
+LEFT JOIN stg_jaffle_shop__customers as customers
+    ON orders.customer_id = customers.customer_id
 LEFT JOIN int_successeful_payments__grouped_by_order_id as payments 
     ON orders.order_id = payments.order_id
+
+)
+
+SELECT * FROM orders__payment_success
